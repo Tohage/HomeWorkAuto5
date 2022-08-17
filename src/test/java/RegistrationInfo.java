369dff7d5c776b50bdc187f731a90018257ea1a2
tohage <avgorin89@gmail.com>
2022-08-17 11:21:32 +0300

    test first task

---
+++ src/test/java/RegistrationInfo.java
@@ -0,0 +1,12 @@
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@RequiredArgsConstructor
+public class RegistrationInfo {
+    private final String city;
+    private final String name;
+    private final String phone;
+}
